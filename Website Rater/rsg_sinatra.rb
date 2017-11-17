e7ea6146b9072363b2742613e325eed96227ea84
Abdullah Alrowais <aalrowais@hotmail.com>
2017-11-17 13:11:07 -0500

    updated

--- Website Rater/rsg_sinatra.rb
+++ Website Rater/rsg_sinatra.rb
@@ -1,20 +1,71 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
 
-session = 0
+
+
 get '/' do
+  @session = 1
   if(session != 0)
+    @user = 1
     erb :home
   else
     erb :signin
   end
 end
 
-get '/:name' do
+get '/Home' do
+  @session = 1
   if(session != 0)
+    @user = 1
     erb :home
   else
     erb :signin
   end
+end
+
+get '/Members' do
+  @session = 1
+  if(session != 0)
+    @user = 1
+    erb :members
+  else
+    erb :signin
+  end
+end
+
+get '/Analytics' do
+  @session = 1
+  if(session != 0)
+    @user = 1
+    erb :analytics
+  else
+    erb :signin
+  end
+end
+
+get '/Uploads' do
+  @session = 1
+  if(session != 0)
+    @user = 1
+    erb :upload
+  else
+    erb :signin
+  end
+end
+
+get '/Sign in' do
+  @session = 1
+  if(session != 0)
+    @user = 1
+    erb :home
+  else
+    erb :signin
+  end
+end
+
+get '/Sign out' do
+  @session = 0
+  @user = 1
+  erb :signin
 end
 
